8f1b47517b5066229fad87e38f5917698972b847
sherwintorrejas <125838306+sherwintorrejas@users.noreply.github.com>
2024-05-18 09:03:59 +0800

    registrationcontrol.php

--- control/registrationcontrol.php
+++ control/registrationcontrol.php
@@ -3,13 +3,13 @@ include_once('../models/registrationmodel.php');
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Create an instance of the RegistrationModel
+
     $registrationModel = new RegistrationModel();
 
-    // Call the registerUser method with the form data
+   
     $registrationModel->registerUser($_POST);
     
-    // Redirect to the login page after registration
+
     header("Location: ../view/login.php");
     exit(); 
 }
